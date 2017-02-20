f8d817b7cb999841df0302783a4f341f93899b89
Fahd Sultan <sfsultan@gmail.com>
2017-02-20 11:51:03 +0500

    Initial Commits

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller{
+
+    public function createCar(Request $request){
+
+        $car = Car::create($request->all());
+
+        return response()->json($car);
+
+    }
+
+    public function updateCar(Request $request, $id){
+
+        $car  = Car::find($id);
+        $car->make = $request->input('make');
+        $car->model = $request->input('model');
+        $car->year = $request->input('year');
+        $car->save();
+
+        return response()->json($car);
+    }
+
+    public function deleteCar($id){
+        $car  = Car::find($id);
+        $car->delete();
+
+        return response()->json('Removed successfully.');
+    }
+
+    public function index(){
+
+        $orders  = Order::all();
+
+        return response()->json($orders);
+
+    }
+}
