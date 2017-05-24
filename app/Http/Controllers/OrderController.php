2d624ff159bc6ff039006e1f32d8568cc7f27fd6
Fahd Sultan <sfsultan@gmail.com>
2017-05-24 10:50:47 +0500

    misc changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,31 +8,25 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller{
 
-    public function createCar(Request $request){
+    // public function createCar(Request $request){
 
-        $car = Car::create($request->all());
+    //     $car = Car::create($request->all());
 
-        return response()->json($car);
+    //     return response()->json($car);
 
-    }
+    // }
 
-    public function updateCar(Request $request, $id){
+    // public function updateCar(Request $request, $id){
 
-        $car  = Car::find($id);
-        $car->make = $request->input('make');
-        $car->model = $request->input('model');
-        $car->year = $request->input('year');
-        $car->save();
+    //     $car  = Car::find($id);
+    //     $car->make = $request->input('make');
+    //     $car->model = $request->input('model');
+    //     $car->year = $request->input('year');
+    //     $car->save();
 
-        return response()->json($car);
-    }
+    //     return response()->json($car);
+    // }
 
-    public function deleteCar($id){
-        $car  = Car::find($id);
-        $car->delete();
-
-        return response()->json('Removed successfully.');
-    }
 
     public function index() {
 
